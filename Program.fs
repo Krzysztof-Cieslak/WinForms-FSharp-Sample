f7d8ac4ab027276cdc58769b2d8493eb2ebc15ee
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-05-26 16:24:16 +0200

    Init

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Windows.Forms
+open MyForms
+
+[<EntryPoint>]
+let main argv =
+    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new Form1());
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,4 @@
+Sample application using .Net Core 3.1, WinForms and F#.
+
+### How to run
+1. `dotnet run`
